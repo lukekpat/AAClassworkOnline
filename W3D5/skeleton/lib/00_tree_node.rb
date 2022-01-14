a4bfbc86ead227164b81ef629ef9128b2582d202
SVKratzer <lukekpat@gmail.com>
2022-01-14 14:19:48 -0500

    Adds init and getter methods

--- W3D5/skeleton/lib/00_tree_node.rb
+++ W3D5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,14 @@
+# Kevin and Luke - PolyTreeNode 
+
 class PolyTreeNode
 
+    attr_reader :parent, :children, :value
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    
 end

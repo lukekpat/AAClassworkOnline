4cdd24bac69f59b6fe6613b12bb8558873e01d9d
SVKratzer <lukekpat@gmail.com>
2022-01-14 18:19:01 -0500

    Adds build move tree

--- W3D5/KnightPathFinder.rb
+++ W3D5/KnightPathFinder.rb
@@ -1,6 +1,18 @@
 require_relative "./skeleton/lib/00_tree_node.rb"
 
-class KnightParthFinder 
+class KnightPathFinder 
+
+  attr_reader :start_pos 
+
+  def initialize(start_pos)
+    @start_pos = start_pos 
+    
+    build_move_tree
+  end
+
+  def build_move_tree
+    self.root_node = PolyTreeNode.new(start_pos)
+
 
 
 

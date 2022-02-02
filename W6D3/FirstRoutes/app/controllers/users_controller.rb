ad628c610736e8ef84c2253eb63469a4ef5e2f66
SVKratzer <lukekpat@gmail.com>
2022-02-02 18:42:40 -0500

    Adds all 8 rest actions

---
+++ W6D3/FirstRoutes/app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+
+  def index
+    render json: User.all 
+  end 
+
+  #localhost:3000/users?user[name]=bob&user[email]=bob@email.com
+  #Sample POST request 
+  def create
+    user = User.new(user_params)
+    # replace the `user_attributes_here` with the actual attribute keys
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    	if user.update(user_params)   
+        # USE DOUBLE QUOTES   VVVVVVV
+		    redirect_to "/users/#{user.id}"
+	    else
+		    render json: user.errors.full_messages, status: 422
+	    end
+  end
+
+  def destroy
+    users = User.find(params[:id])
+    User.find(params[:id]).destroy 
+    render json: users 
+
+  end
+
+  private
+  def user_params
+	  params.require(:user).permit(:name, :email)
+  end
+
+end

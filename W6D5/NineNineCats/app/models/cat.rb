ec657160d557eec3607bd7bd91cc70c77ba5e89c
SVKratzer <lukekpat@gmail.com>
2022-02-04 16:22:06 -0500

    Adds model cat

---
+++ W6D5/NineNineCats/app/models/cat.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :integer          not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+  COLORS = [:brown, :white, :black, :gray]
+  validates :color, inclusion: COLORS
+  validates :birth_date, :color, :name, :sex, presence: true 
+  validates :sex, inclusion: ["M", "F"]
+
+  def age
+    now = Time.now.utc.to_date 
+    dob = self.birth_date
+    now.year - dob.year
+  end
+end

8477cb75ccf968b6d354f9ba301de6f0b7a0a578
kingbloopy <kingbloopy@gmail.com>
2022-02-11 14:36:00 -0800

    controlla

--- W7D5/RedditClone/app/controllers/application_controller.rb
+++ W7D5/RedditClone/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  helper_method :require_logged_in, :current_user, :logged_in?
   
   def current_user
     @current_user ||= User.find_by(session_token: session[:session_token])
@@ -6,6 +7,10 @@ class ApplicationController < ActionController::Base
 
   def require_logged_in
     redirect_to new_session_url if !logged_in?
+  end
+
+  def require_logged_out
+    redirect_to user_url(current_user) if logged_in?
   end
 
   def login(user)
--- W7D5/RedditClone/db/schema.rb
+++ W7D5/RedditClone/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_02_11_203821) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "email", null: false
